481de58b35d0365b86569f2ffcbe0a92215ed528
blainefreestone <blainefreestone01@gmail.com>
2023-09-17 12:21:05 -0700

    js creates divs in a 16x16 grid with proper data tags and column divs to organize.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const container = document.querySelector('#container');
+
+for (let column = 0; column < 16; column++)
+{
+    let columnDiv = document.createElement('div');
+    columnDiv.classList.add('column');
+    columnDiv.setAttribute('data-column', column);
+    console.log(columnDiv);
+
+    for (let row = 0; row < 16; row++)
+    {
+        let gridDiv = document.createElement('div');
+        gridDiv.classList.add('grid-square');
+        gridDiv.setAttribute('data-column', column);
+        gridDiv.setAttribute('data-row', row);
+        console.log(gridDiv);
+        columnDiv.appendChild(gridDiv);
+    }
+
+    container.appendChild(columnDiv);
+}
