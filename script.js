a7c15cffe13ca989d493a7543116f44f137ade70
blainefreestone <blainefreestone01@gmail.com>
2023-09-17 21:26:28 -0700

    event listeners added to grid-squares which call the updateGridBox function to make it black.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('#container');
+const gridDivs = [];
 
 for (let column = 0; column < 16; column++)
 {
     let columnDiv = document.createElement('div');
     columnDiv.classList.add('column');
     columnDiv.setAttribute('data-column', column);
-    console.log(columnDiv);
 
     for (let row = 0; row < 16; row++)
     {
@@ -13,9 +13,16 @@ for (let column = 0; column < 16; column++)
         gridDiv.classList.add('grid-square');
         gridDiv.setAttribute('data-column', column);
         gridDiv.setAttribute('data-row', row);
-        console.log(gridDiv);
         columnDiv.appendChild(gridDiv);
+        gridDivs.push(gridDiv);
     }
 
     container.appendChild(columnDiv);
 }
+
+function updateGridBox(e)
+{
+    e.target.classList.add('colored');
+}
+
+gridDivs.forEach(gridBox => gridBox.addEventListener('mouseover', updateGridBox));
